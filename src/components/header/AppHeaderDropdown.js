481b3e4e31119bf001b2352fd2602d3aa8584684
Mohsin <follow.mohsin@gmail.com>
2024-02-08 14:43:19 +0600

    profile logo update

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 import {CAvatar,CDropdown,CDropdownDivider,CDropdownHeader,CDropdownItem,CDropdownMenu,CDropdownToggle,} from '@coreui/react'
 import {cilLockLocked,cilSettings,cilUser,} from '@coreui/icons'
@@ -9,12 +9,34 @@ import { supabase } from '../../helper/supabaseClient';
 const AppHeaderDropdown = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState([])
   
   const logout = async () => {
       setUser(null);
       localStorage.removeItem('supabaseSession');
       await supabase.auth.signOut();
   };
+
+  useEffect(() => {
+    fetchProfile()
+  }, [])
+
+  const fetchProfile = async () => {
+    try {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+      const { data, error } = await supabase.from('profiles').select('*').eq('id', user.id).single()
+
+      if (error) {
+        throw error
+      }
+
+      setProfile(data || [])
+    } catch (error) {
+      console.error('Error fetching profile:', error.message)
+    }
+  }
   
   const goToProfile = () => {
       navigate('/profile');
@@ -23,7 +45,7 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-        <CAvatar src={avatar9} size="md" />
+        <CAvatar src={profile.avatar_url} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
